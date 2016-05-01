9f99d4aaef5156e090979b42b3db78bc150e09b4
Tim Weber <scy@scy.name>
2016-05-01 11:11:08 +0200

    Update /contact/ a bit
    That was overdue. Actually, doing the whole site from scratch is overdue …

--- contact/index.php
+++ contact/index.php
@@ -3,7 +3,7 @@
 <p>
 	<?php i18n(array('en' => 'My <strong>e-mail</strong> address is:', 'de' => 'Meine <strong>E-Mail-Adresse</strong> lautet:')); ?>
 </p>
-<p style='text-align:center; font-style:italic; font-size:150%; color:#8f4;'>s<!-- confused yet, spam bot? -->c<span class='sondela'><!-- maybe now. --></span>y-e<?php echo(($expire = date('ymd', time() + 604800))); ?>&#x40;scytale.name</p>
+<p style='text-align:center; font-style:italic; font-size:150%; color:#8f4;'>s<!-- confused yet, spam bot? -->c<span class='sondela'><!-- maybe now. --></span>y-e<?php echo(($expire = date('ymd', time() + 604800))); ?>&#x40;scy.name</p>
 <p><?php i18n(array('en' => '
 	This address will <strong>expire</strong> in one week, starting from now, to protect me from spam.
 	If you write to me, I will reply from my real address which does not expire.
@@ -22,35 +22,13 @@
 	Mir wenn nötig eine anonyme Mail zu schicken ist kein Problem, aber benutzen Sie bitte eine Mail-Adresse, die Antworten empfangen kann, selbst wenn ich nicht antworten muss:
 	Die Absenderadresse eingehender Mails wird automatisch auf Existenz geprüft, und wenn sie keine Antworten akzeptiert, wird die Mail zum Schutz vor Spam von gefälschten Absenderadressen automatisch verworfen.
 ')); ?></p>
-<p><?php i18n(array('en' => '
-	My <strong>Jabber/XMPP</strong> address (also usable with Google Talk) is <em>scy@jabber.ccc.de</em>.
-	I only authorize people I’m actually interested in talking to.
-	Don’t argue.
-	If you are not 100% sure that I will know from your Jabber ID or nickname (and I might only be seeing one of them) who you are, please announce your intention to add me in advance via some other communication channel.
-', 'de' => '
-	Meine <strong>Jabber/XMPP-Adresse</strong> (auch mit Google Talk benutzbar) ist <em>scy@jabber.ccc.de</em>.
-	Ich autorisiere nur Leute, mit denen ich tatsächlich reden möchte.
-	Nicht verhandelbar.
-	Sollten Sie sich nicht hundertprozentig sicher sein, dass ich anhand der Jabber-ID oder dem Nickname (und womöglich sehe ich nur eins von beiden) weiß, wer Sie sind, kündigen Sie Ihre Autorisierungsanfrage bitte vorher über einen anderen Kommunikationskanal an.
-')); ?></p>
-<p><?php i18n(array('en' => '
-	You can leave me a <strong>telephonic voice message</strong> by calling <var class=\'phone\'>+49&nbsp;621&nbsp;54&nbsp;55&nbsp;56&nbsp;02</var>.
-	I\'ll receive this message instantly via e-mail and get in touch with you as soon as possible.
-	Please understand that, because of possible abuse, I can only publish a voice mail number here.
-	I reserve the right to publish abusive calls.
-', 'de' => '
-	Sie können mir eine <strong>telefonische Sprachnachricht</strong> unter der Nummer <var class=\'phone\'>+49&nbsp;(0)621&nbsp;54&nbsp;55&nbsp;56&nbsp;02</var> hinterlassen.
-	Ich erhalte diese Nachricht sofort per E-Mail und werde mich so schnell wie möglich mit Ihnen in Verbindung setzen.
-	Bitte haben Sie Verständnis dafür, dass ich aufgrund des Missbrauchspotentials hier nur eine Mailboxnummer veröffentlichen kann.
-	Ich behalte mir vor, missbräuchliche Anrufe zu veröffentlichen.
-')); ?></p>
 <p>
 	<?php i18n(array('en' => 'I receive <strong>"normal" mail</strong> at the following address:', 'de' => '<strong>"Normale" Post</strong> empfange ich an folgender Adresse:')); ?>
 </p>
 <pre class='postal' style='clear:both;'>
 Tim Weber
-U&nbsp;5, 4
-68161 Mannheim
+Bethmannstr.&nbsp;17
+60311 Frankfurt am Main
 Germany
 </pre>
 <p><?php i18n(array('en' => '
@@ -61,9 +39,9 @@ Germany
 	<del>Ich bin <em>Scytale</em> auf Freenode, OFTC und Hackint, aber <em>Scyta1e</em> (mit der Zahl "eins" statt einem "L") im IRCNet.</del>
 ')); ?></p>
 <p><?php i18n(array('en' => '
-	Additionally, I am <a href="https://twitter.com/Scytale">Scytale on Twitter</a>, <a href="http://www.last.fm/user/Scy83">Scy83 on Last.fm</a> and <a href="http://www.facebook.com/Scy83">on Facebook</a>.
+	Additionally, I am <a href="https://twitter.com/scy">scy on Twitter</a>, <a href="http://www.last.fm/user/Scy83">Scy83 on Last.fm</a> and <a href="http://www.facebook.com/Scy83">on Facebook</a>.
 ', 'de' => '
-	Darüberhinaus bin ich <a href="https://twitter.com/Scytale">Scytale auf Twitter</a>, <a href="http://www.last.fm/user/Scy83">Scy83 bei Last.fm</a> und <a href="http://www.facebook.com/Scy83">in Facebook</a>.
+	Darüberhinaus bin ich <a href="https://twitter.com/scy">scy auf Twitter</a>, <a href="http://www.last.fm/user/Scy83">Scy83 bei Last.fm</a> und <a href="http://www.facebook.com/Scy83">in Facebook</a>.
 ')); ?></p>
 <h2><?php i18n(array('en' => 'Imprint', 'de' => 'Impressum')); ?></h2>
 <p><?php i18n(array('en' => '
