50ba8df0fa9cac04184ba92723b52723640e04bf
Tim Weber <scy@scy.name>
2019-03-16 12:21:09 +0100

    Remove Google Analytics

--- inc/scytale.name.php
+++ inc/scytale.name.php
@@ -87,19 +87,6 @@ function ftr() {
 		></div>
 	</div>
 	<div id='RIGHT'><div id='AVATAR'><!-- <a href='/me/' title='<?php i18n(array('en' => 'about me', 'de' => 'über mich')); ?>'> --><img src='/style/07/scytale.png' width='48' height='48' alt='Scytale, <?php i18n(array('en' => 'avatar image', 'de' => 'Avatarbild')); ?>' /><!-- </a> --></div></div>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-23544141-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </body>
 
 </html><?php
